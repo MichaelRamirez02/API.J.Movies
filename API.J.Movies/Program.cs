1e62ee65dbed2bdfb6546547d6e8964370916d27
MichaelRamirez <maicol.90.rami@gmail.com>
2025-11-26 14:04:24 -0500

    New CategoriesController and CategoryService Created

--- API.J.Movies/Program.cs
+++ API.J.Movies/Program.cs
@@ -1,12 +1,22 @@
 using API.J.Movies.DAL;
+using API.J.Movies.MoviesMapper;
+using API.J.Movies.Repository;
+using API.J.Movies.Repository.IRepository;
+using API.J.Movies.Services;
+using API.J.Movies.Services.IServices;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
+builder.Services.AddAutoMapper(cfg => cfg.AddProfile<Mappers>());
 
+//Services
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
+//Repositories
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
