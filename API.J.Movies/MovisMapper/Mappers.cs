bfe92ea90010eb3e99f616d8ff531e763a3d0a56
MichaelRamirez <maicol.90.rami@gmail.com>
2025-11-26 13:37:54 -0500

    Create New Mappers and Dto's

---
+++ API.J.Movies/MovisMapper/Mappers.cs
@@ -0,0 +1,16 @@
+﻿using API.J.Movies.DAL.Dtos.Category;
+using API.J.Movies.DAL.Models;
+using AutoMapper;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace API.J.Movies.MoviesMapper
+{
+    public class Mappers : Profile
+    {
+        public Mappers()
+        {
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Category, CategoryCreateUpdateDto>().ReverseMap();
+        }
+    }
+}
